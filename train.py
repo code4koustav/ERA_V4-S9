c449dfd02ba98d4f9f0112056e15e806a40dcfd6
Smita Sasindran <smita@smarthomesentry.com>
2025-10-12 20:14:07 +0530

    Basic train and test loops

---
+++ train.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+
+
+def train_loop(model, device, train_loader, optimizer, train_losses, train_acc):
+    """
+    Training loop for one epoch
+    """
+    # ToDo Smita: Add loss function as inp + helpers for LRfinder/Onecycle LR
+    # early stopping, checkpoints in main.py loop -> add helper fns
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
+
+        # Init
+        optimizer.zero_grad()
+
+        # Predict
+        y_pred = model(data)
+
+        # Calculate loss
+        loss = F.nll_loss(y_pred, target)
+        train_losses.append(loss)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        # Update pbar-tqdm
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+
+        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+        train_acc.append(100*correct/processed)
+
+    return train_losses, train_acc
+
+
+def test_loop(model, device, test_loader, test_losses, test_acc):
+    """
+    Test loop for one epoch
+    """
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    test_acc.append(100. * correct / len(test_loader.dataset))
+
+    return test_losses, test_acc
+
+
